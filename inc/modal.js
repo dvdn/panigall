f2858431a9e36cdc977615264128049b497338de
dvdn <dvdngrllo@gmx.com>
2024-04-21 22:55:48 +0200

    Modal NextPrev, init

--- inc/modal.js
+++ inc/modal.js
@@ -7,31 +7,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var modalImg = document.getElementById("js-modal-content");
     var captionText = document.getElementById("caption");
 
-    for (var i = 0; i < itemImg.length; i++) {
-        console.log(itemImg[i]);
-        itemImg[i].addEventListener("click", (event) => {
+    var position = document.getElementById("img_pos");
+    var next = document.getElementById("next");
+    var prev = document.getElementById("prev");
 
-            if (!this.activeElement.getAttribute('href')) return; // Not found. Exit here.
+    for (var i = 0; i < itemImg.length; i++) {
+
+        itemImg[i].addEventListener("click", (event) => {
+            $currentElmnt = event.currentTarget;
+            if (!$currentElmnt.getAttribute('href')) return; // Not found, exit here
 
             modal.style.display = "block";
-            modalImg.src = this.activeElement.getAttribute('href');
-            var itemName = this.activeElement.childNodes[1].innerHTML; // Span contains item name
+            modalImg.src = $currentElmnt.getAttribute('href');
+            var itemName = $currentElmnt.childNodes[1].innerHTML; // Span contains item name
             captionText.innerHTML = itemName;
+            position.innerHTML = [...itemImg].indexOf($currentElmnt);
 
             event.preventDefault();
             return false;
-
         });
-
     }
 
     // Closes the modal
-    modal.onclick = function() {
-        modal.style.display = "none";
-    }
+    // modal.onclick = function() {
+    //     modal.style.display = "none";
+    // }
     var span = document.getElementsByClassName("close")[0];
     span.onclick = function() {
         modal.style.display = "none";
     }
+
+    // Next Prev
+    next.addEventListener("click", (e) => {
+        var currentPos = Number(position.innerHTML)
+        if (currentPos < itemImg.length-1) {
+            itemImg[currentPos+1].click();
+        } else {
+            modal.style.display = "none";
+        }
+    });
+
+    prev.addEventListener("click", (e) => {
+        var currentPos = Number(position.innerHTML)
+        if (currentPos > 0) {
+            itemImg[currentPos-1].click();
+        } else {
+            modal.style.display = "none";
+        }
+    });
 
 });
--- inc/modal.php
+++ inc/modal.php
@@ -1,5 +1,8 @@
 <div id="js-modal" class="modal">
     <span class="close" title="close">&times;</span>
+    <div id="next">Next</div>
+    <div id="prev">Prev</div>
     <img id="js-modal-content" class="modal-content" >
     <div id="caption"></div>
+    <span id="img_pos"></span>
 </div>
